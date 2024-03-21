fa9c17cb4ce95a8f5f936d03d8e6f807dde600ab
Joe Quintero <joedarwin1998@gmail.com>
2024-03-21 19:59:37 -0400

    Primera prueba con condicional

--- cypress/e2e/Asserts.cy.js
+++ cypress/e2e/Asserts.cy.js
@@ -1,7 +1,23 @@
+
 describe("Practicas de Asserts ", ()=>{
 
     it("Asert find_eq ", ()=>{
-        cy.visit("https://opencart.abstracta.us/");
+        cy.visit("http://vol.auditoria.venezolano.com/");
+
+        cy.get("#datoLogin").type("jdquintero");
+        cy.get("#clave").type("abc123");
+        cy.get('#aceptar').click();
+
+        cy.get(':nth-child(1) > .btn > .row > .col-9').then((e) => {
+            let nombre=e.text();
+            cy.log(nombre);
+            if (nombre=="hola juan") {
+                cy.get(':nth-child(1) > .btn').click();
+            }else{
+                cy.get('#qrCliente > .fa-solid').click();
+            }
+        })
+
     })
 
 })
